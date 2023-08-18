63482746d5b3fa8db3f2f04268703e656280f668
MashaToroptseva <toroptseva@supplerus.com>
2023-08-18 18:23:16 +0300

    add step 1

--- src/panels/Step1.js
+++ src/panels/Step1.js
@@ -7,21 +7,48 @@ import {
   PanelHeaderBack,
   Div,
   Button,
+  Input,
+  ButtonGroup,
 } from "@vkontakte/vkui";
 
 import "./index.css";
+import account from "../img/account.png";
 
 const Step1 = ({ id, go }) => (
   <Panel id={id}>
     <PanelHeader before={<PanelHeaderBack onClick={go} data-to="menu" />}>
       Мой Газ
     </PanelHeader>
-    <Div>
-      <Panel>
-        <Button size="l" appearance="accent" stretched>
-          Оплатить
-        </Button>
-      </Panel>
+    <Div className="menu-container">
+      <Div className="msg center">
+        <div>
+          <img className="account-img" src={account} />
+        </div>
+        <div>
+          Введите номер лицевого счета (9 цифр). Номер лицевого счета можно
+          посмотреть в квитанции.
+        </div>
+      </Div>
+      <div>
+        <div className="input">
+          <div className="input-title">Введите лицевой счёт</div>
+          <Input id="account-number" type="number" max={9} />
+        </div>
+        <ButtonGroup className="button-group" mode="vertical" gap="m" stretched>
+          <Button size="l" appearance="accent" stretched>
+            Продолжить
+          </Button>
+          <Button
+            size="l"
+            appearance="accent"
+            stretched
+            onClick={go}
+            data-to="menu"
+          >
+            Назад
+          </Button>
+        </ButtonGroup>
+      </div>
     </Div>
   </Panel>
 );
